ed94b75fc0ab39d36ecd944553c16483331a8789
zkjon <121103151+zkj0n@users.noreply.github.com>
2025-09-05 10:48:56 +0200

    feat: add Header component with game description and title

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+const Header = () => (
+    <header>
+        <h1 className="text-4xl font-medium underline w-fit">
+            el juego del ahorcado
+        </h1>
+        <p className="mt-4 text-2xl font-ligth max-w-2xl text-justify">
+            Bienvenido al juego del ahorcado, donde puedes poner a prueba tus
+            habilidades de adivinanza y vocabulario. El objetivo del juego es
+            adivinar la palabra oculta antes de que se complete el dibujo del
+            ahorcado, tardará <strong className={`underline`}>6 intentos</strong> en completarse.
+        </p>
+    </header>
+);
+
+export default Header;
