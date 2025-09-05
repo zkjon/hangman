dc317f58be9b367d9d5aa28902654434ac367dc3
zkjon <121103151+zkj0n@users.noreply.github.com>
2025-09-05 11:17:22 +0200

    fix: correct capitalization of game title in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => (
     <header>
         <h1 className="text-4xl font-medium underline w-fit">
-            el juego del ahorcado
+            El juego del ahorcado
         </h1>
         <p className="mt-4 text-2xl font-ligth max-w-2xl text-justify">
             Bienvenido al juego del ahorcado, donde puedes poner a prueba tus
