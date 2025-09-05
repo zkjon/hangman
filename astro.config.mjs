1629c540bafea0f353d6080faed22423a2440b95
zkjon <121103151+zkj0n@users.noreply.github.com>
2025-09-05 09:50:28 +0200

    chore: configure Vite with Tailwind CSS and update TypeScript paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import path from 'path';
+
+import tailwind from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [
+      tailwind()
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
+});
